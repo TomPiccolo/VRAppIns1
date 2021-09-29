8eae31233b8d634191937db7972f938dec3a950d
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2021-09-29 17:50:09 -0400

    SuccessfulRebuildNewProjectVR2
    Reverted to passed successful build and changed project name of app for build

--- InspireVR1/Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/DefaultCompany/InspireVR2/BuildConfig.java
+++ InspireVR1/Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/DefaultCompany/InspireVR2/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.DefaultCompany.InspireVR1;
+package com.DefaultCompany.InspireVR2;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.DefaultCompany.InspireVR1";
+  public static final String APPLICATION_ID = "com.DefaultCompany.InspireVR2";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "0.1";
