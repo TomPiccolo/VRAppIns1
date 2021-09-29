9a5f85fdc9f72c0b0eaf65540484f01e1899daf4
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2021-09-29 10:59:35 -0400

    WouldNotRune

--- InspireVR1/Assets/HandPresence.cs
+++ InspireVR1/Assets/HandPresence.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
+
 public class HandPresence : MonoBehaviour
 {
     public bool showController = false;
@@ -18,6 +19,11 @@ public class HandPresence : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        TryInitialize();
+    }
+
+    void TryInitialize()
+    {
         List<InputDevice> devices = new List<InputDevice>();
         InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
@@ -25,7 +31,7 @@ public class HandPresence : MonoBehaviour
         {
             Debug.Log("item.name + item.characteristics");
         }
-        if(devices.Count > 0)
+        if (devices.Count > 0)
         {
             targetDevice = devices[0];
             GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
@@ -42,48 +48,52 @@ public class HandPresence : MonoBehaviour
             spawnedHandModel = Instantiate(handModelPrefab, transform);
             handAnimator = spawnedHandModel.GetComponent<Animator>();
         }
-            
+
+        void UpdateHandAnimation()
+        {
+            if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+            {
+                handAnimator.SetFloat("Trigger", triggerValue);
+            }
+
+            else
+            {
+                handAnimator.SetFloat("Trigger", 0);
+            }
+            if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+            {
+                handAnimator.SetFloat("Grip", gripValue);
+            }
+
+            else
+            {
+                handAnimator.SetFloat("Grip", 0);
+            }
+        }
+
+
+
+        if (targetDevice.isValid)
+            {
+                TryInitialize();
+            }
+            else
+            {
+                if (showController)
+                {
+                    spawnedHandModel.SetActive(false);
+                    spawnedController.SetActive(true);
+                }
+                else
+                {
+                    spawnedHandModel.SetActive(true);
+                    spawnedController.SetActive(false);
+                    UpdateHandAnimation();
+                }
+
+            }
+        }
     }
-
-    void UpdateHandAnimation()
-    {
-        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
-        {
-            handAnimator.SetFloat("Trigger", triggerValue);
-        }
-
-        else
-        {
-            handAnimator.SetFloat("Trigger", 0);
-        }
-        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
-        {
-            handAnimator.SetFloat("Grip", gripValue);
-        }
-
-        else
-        {
-            handAnimator.SetFloat("Grip", 0);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-
-        if(showController)
-        {
-            spawnedHandModel.SetActive(false);
-            spawnedController.SetActive(true);
-           
-        }
-        else
-        {
-            spawnedHandModel.SetActive(true);
-            spawnedController.SetActive(false);
-            UpdateHandAnimation();
-        }
-    }
-}
+       
+    
 
