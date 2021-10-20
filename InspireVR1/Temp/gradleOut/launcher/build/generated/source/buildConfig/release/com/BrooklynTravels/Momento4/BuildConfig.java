b555885fd72feb9a17ae20f9f31380edf203c38b
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2021-10-20 11:42:12 -0400

    DeletedMovieQuads
    made them Prefabs and deleted them from present scene

--- InspireVR1/Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/BrooklynTravels/Momento4/BuildConfig.java
+++ InspireVR1/Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/BrooklynTravels/Momento4/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.BrooklynTravels.Momento3;
+package com.BrooklynTravels.Momento4;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.BrooklynTravels.Momento3";
+  public static final String APPLICATION_ID = "com.BrooklynTravels.Momento4";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "0.1";
